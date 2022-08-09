9d091068b7cc6366e5a426743954d97097e7e6ea
Fábio Mateus Melo <fabioceto@gmail.com>
2022-08-09 12:51:23 -0300

    0011

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import './index.css'
 import { GiFilmSpool } from 'react-icons/gi'
-import { BiTrash } from 'react-icons/bi'
 import Search from './componentes/buscar'
 import Addfilms from './componentes/Addfilms'
 import appointmentList from './componentes/data.json'
+import AppointmentInfo from './componentes/Appointmentinfo'
 
-export default function App() {
+function App() {
   return (
     <div className="App container mx-auto mt-5 font-thin">
-      <h1 className="text-5xl text-blue-700 mb-3">FilmsG
-      <GiFilmSpool className="inline-block text-blue-500" /></h1>
+      <h1 className="text-5xl text-blue-700 mb-3">
+        FilmsG
+        <GiFilmSpool className="inline-block text-blue-500" />
+      </h1>
       <Addfilms />
       <Search />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList
-          .map(appointment => (
-            <li className="px-3 py-3 flex items-start">
-              <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <BiTrash /></button>
-              <div className="flex-grow">
-                <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-                  <span className="flex-grow text-right">{appointment.aptDate}</span>
-                </div>
-                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-                <div className="leading-tight">{appointment.aptNotes}</div>
-              </div>
-            </li>
-          ))
-        }
+        {appointmentList.map(appointment => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
       </ul>
     </div>
   )
 }
 
-
+export default App
--- src/componentes/Addfilms.js
+++ src/componentes/Addfilms.js
@@ -1,71 +1,116 @@
-import { GiFilmSpool } from "react-icons/gi";
+import { GiFilmSpool } from 'react-icons/gi'
+import { useState } from 'react'
 
 const Addfilms = () => {
+  let [toggleForm, setToggleForm] = useState(false)
   return (
     <div>
-      <button className="bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md">
-        <div><GiFilmSpool className="inline-block" />  Add Film</div>
+      <button onClick={() => { setToggleForm(!toggleForm) }}
+      className={`bg-blue-400 text-white px-3 py-2 w-full text-left rounded-t-md
+      ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`}>
+        <div>
+          <GiFilmSpool className="inline-block" /> Add Film
+        </div>
       </button>
-      <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-          <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-            Owner Name
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input type="text" name="ownerName" id="ownerName"
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"/>
+      {toggleForm && (
+        <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label
+              htmlFor="ownerName"
+              className="block text-sm font-medium text-gray-300 sm:mt-px sm:pt-0"
+            >
+              Título
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="ownerName"
+                id="ownerName"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"
+              />
+            </div>
+          </div>
+
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label
+              htmlFor="petName"
+              className="block text-sm font-medium text-gray-300 sm:mt-px sm:pt-0"
+            >
+              Autor
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="text"
+                name="petName"
+                id="petName"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"
+              />
+            </div>
+          </div>
+
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label
+              htmlFor="aptDate"
+              className="block text-sm font-medium text-gray-300 sm:mt-px sm:pt-0"
+            >
+              Data de Lançamento
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="date"
+                name="aptDate"
+                id="aptDate"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"
+              />
+            </div>
+          </div>
+
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label
+              htmlFor="aptTime"
+              className="block text-sm font-medium text-gray-300 sm:mt-px sm:pt-0"
+            >
+              Duração
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <input
+                type="time"
+                name="aptTime"
+                id="aptTime"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"
+              />
+            </div>
+          </div>
+
+          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
+            <label
+              htmlFor="aptNotes"
+              className="block text-sm font-medium text-gray-300 sm:mt-px sm:pt-1"
+            >
+              Sinopse
+            </label>
+            <div className="mt-1 sm:mt-0 sm:col-span-2">
+              <textarea
+                id="aptNotes"
+                name="aptNotes"
+                rows="3"
+                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="pt-5">
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-sm text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+              >
+                Enviar
+              </button>
+            </div>
           </div>
         </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-          <label htmlFor="petName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-            Pet Name
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input type="text" name="petName" id="petName"
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0" />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-          <label htmlFor="aptDate" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-            Apt Date
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input type="date" name="aptDate" id="aptDate"
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0" />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-          <label htmlFor="aptTime" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-            Apt Time
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <input type="time" name="aptTime" id="aptTime"
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0" />
-          </div>
-        </div>
-
-        <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-          <label htmlFor="aptNotes" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-            Appointment Notes
-          </label>
-          <div className="mt-1 sm:mt-0 sm:col-span-2">
-            <textarea id="aptNotes" name="aptNotes" rows="3"
-              className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-sm px-1 focus:outline-none focus:ring-2 focus:ring-offset-0"></textarea>
-          </div>
-        </div>
-
-
-        <div className="pt-5">
-          <div className="flex justify-end">
-            <button type="submit" className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-sm text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
-              Submit
-            </button>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   )
 }

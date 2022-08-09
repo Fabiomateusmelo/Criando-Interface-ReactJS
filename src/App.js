1553b7f9e08af6851679aa36928ba9afa02efd42
Fábio Mateus Melo <fabioceto@gmail.com>
2022-08-08 22:15:46 -0300

    0007

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './index.css'
+import { GiFilmSpool } from 'react-icons/gi'
+import Search from './componentes/buscar'
+import Addfilms from './componentes/Addfilms'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className=" bg-red-500"> Olá ! </h1>
-      </header>
+    <div className="App container mx-auto mt-5 font-thin">
+      <h1 className="text-5xl text-blue-700 mb-3">FilmsG
+      <GiFilmSpool className="inline-block text-blue-500" /></h1>
+      <Addfilms />
+      <Search />
     </div>
-  );
-};
+  )
+}
 
-export default App
+
